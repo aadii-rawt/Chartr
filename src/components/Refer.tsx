a6d553be0a0d1edd3b27662a1b339f61f8eedb1b
aadii-rawt <rawatadii060@gmail.com>
2026-09-17 21:56:45 +0530

    banner img

--- src/components/Refer.tsx
+++ src/components/Refer.tsx
@@ -10,10 +10,12 @@ const Refer = () => {
     <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-h-screen h-full flex items-center justify-center bg-black/30'>
      
      <div className='relative'>
-       <RxCross1 className="absolute text-lg right-8 top-5 cursor-pointer" onClick={() => setShowRefer(false)}/>
+       <RxCross1 className="absolute text-xl right-8 top-3 bg-white w-5 cursor-pointer" onClick={() => setTimeout(() => {
+         setShowRefer(false)
+       }, 5000)}/>
         <div onClick={() => navigate("/refer")}>
 
-        <img src="./refer.png" alt="" className='w-full px-5 lg:w-1/2' />
+        <img src="./banner.png" alt="" className='w-full px-5 lg:w-1/2' />
         </div>
       </div> 
     </div>
